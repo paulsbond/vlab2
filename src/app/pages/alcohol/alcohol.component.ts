5fa5ce686b111f0efcbb3e9bf9b860771ed5e3e0
Paul Bond <git@paulsbond.co.uk>
2023-07-22 13:16:17 +0100

    Alcohol service

--- src/app/pages/alcohol/alcohol.component.ts
+++ src/app/pages/alcohol/alcohol.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { DilutionResult } from '../../components/dilution/dilution.component';
 import { random } from '../../utils';
+import { AlcoholService } from './alcohol.service';
 
 @Component({
   selector: 'app-alcohol',
@@ -8,42 +9,12 @@ import { random } from '../../utils';
   styleUrls: ['./alcohol.component.css']
 })
 export class AlcoholComponent {
-  samples = [
-    new Sample("Ethanol 100%", 100, "stock", "ethanol.png"),
-    new Sample("Lager", 2.9, "unknown", "pint.png"),
-    new Sample("IPA", 4.6, "unknown", "pint.png"),
-    new Sample("Stout", 3.6, "unknown", "pint.png"),
-    new Sample("Cider", 5.3, "unknown", "pint.png"),
-    new Sample("Shiraz", 14.2, "unknown", "wine.png"),
-    new Sample("Sauvignon Blanc", 13.6, "unknown", "wine.png"),
-    new Sample("Cabernet Sauvignon", 12.4, "unknown", "wine.png"),
-    new Sample("Pinot Grigio", 13.9, "unknown", "wine.png"),
-    new Sample("Gin", 37.5, "unknown", "spirit.png"),
-    new Sample("Vodka", 40.2, "unknown", "spirit.png"),
-    new Sample("Whisky", 38.5, "unknown", "spirit.png"),
-    new Sample("Tequila", 36.4, "unknown", "spirit.png"),
-    new Sample("Rum & Coke", 4.2, "unknown", "mixer.png"),
-    new Sample("Gin & Tonic", 9.7, "unknown", "mixer.png"),
-    new Sample("Vodka & Diet Coke", 7.4, "unknown", "mixer.png"),
-    new Sample("Schnapps & Lemonade", 6.7, "unknown", "mixer.png"),
-  ];
-  actions = ["Dilution", "Gas Chromatography"];
-  selectedSample = this.samples[0];
-  selectedAction = this.actions[0];
+  constructor(public service: AlcoholService) { }
 
   onDiluted(result: DilutionResult) {
     const sample_volume = result.sample_volume * random(0.99, 1.01);
     const total_volume = result.flask_volume + random(-0.1, 0.1);
-    const conc = this.selectedSample.conc * (sample_volume / total_volume);
-    this.samples.push(new Sample(result.label, conc));
+    const conc = this.service.selectedSample.conc * (sample_volume / total_volume);
+    this.service.addSample(result.label, conc);
   }
-}
-
-class Sample {
-  constructor(
-    public label: string,
-    public conc: number,
-    public type: string = 'dilution',
-    public image: string = 'vial.png'
-  ) { }
 }
---
+++ src/app/pages/alcohol/alcohol.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+
+export class Sample {
+  constructor(
+    public label: string,
+    public conc: number,
+    public type: string = 'dilution',
+    public image: string = 'vial.png'
+  ) { }
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlcoholService {
+  samples = [
+    new Sample("Ethanol 100%", 100, "stock", "ethanol.png"),
+    new Sample("Lager", 2.9, "unknown", "pint.png"),
+    new Sample("IPA", 4.6, "unknown", "pint.png"),
+    new Sample("Stout", 3.6, "unknown", "pint.png"),
+    new Sample("Cider", 5.3, "unknown", "pint.png"),
+    new Sample("Shiraz", 14.2, "unknown", "wine.png"),
+    new Sample("Sauvignon Blanc", 13.6, "unknown", "wine.png"),
+    new Sample("Cabernet Sauvignon", 12.4, "unknown", "wine.png"),
+    new Sample("Pinot Grigio", 13.9, "unknown", "wine.png"),
+    new Sample("Gin", 37.5, "unknown", "spirit.png"),
+    new Sample("Vodka", 40.2, "unknown", "spirit.png"),
+    new Sample("Whisky", 38.5, "unknown", "spirit.png"),
+    new Sample("Tequila", 36.4, "unknown", "spirit.png"),
+    new Sample("Rum & Coke", 4.2, "unknown", "mixer.png"),
+    new Sample("Gin & Tonic", 9.7, "unknown", "mixer.png"),
+    new Sample("Vodka & Diet Coke", 7.4, "unknown", "mixer.png"),
+    new Sample("Schnapps & Lemonade", 6.7, "unknown", "mixer.png"),
+  ];
+  actions = ["Dilution", "Gas Chromatography"];
+  selectedSample = this.samples[0];
+  selectedAction = this.actions[0];
+
+  addSample(label: string, conc: number) {
+    this.samples.push(new Sample(label, conc));
+  }
+}
