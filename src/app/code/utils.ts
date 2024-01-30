363f26def59e837b50309ca1ea2a97b235299276
Paul Bond <git@paulsbond.co.uk>
2024-01-30 11:31:26 +0000

    Faster UV-Vis lookup

--- src/app/code/utils.ts
+++ src/app/code/utils.ts
@@ -6,3 +6,8 @@ export function decimal_places(x: number): number {
   if (Math.floor(x.valueOf()) === x.valueOf()) return 0;
   return x.toString().split('.')[1].length || 0;
 }
+
+export function round(value: number, ndigits: number = 0): number {
+  var multiplier = Math.pow(10, ndigits);
+  return Math.round(value * multiplier) / multiplier;
+}
