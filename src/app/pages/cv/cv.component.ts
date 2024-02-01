5abc23a22e36489ae5058597c7bedac9ca76417b
Paul Bond <git@paulsbond.co.uk>
2024-02-01 12:24:26 +0000

    CV form

--- src/app/pages/cv/cv.component.ts
+++ src/app/pages/cv/cv.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { CvService } from './cv.service';
 
 @Component({
   selector: 'app-cv',
   templateUrl: './cv.component.html',
   styleUrls: ['./cv.component.css']
 })
-export class CvComponent {
+export class CvComponent implements OnInit {
+  public error: string = '';
+  @ViewChild('form', { static: true }) form!: NgForm;
+  formChangesSubscription: any;
 
+  constructor(public service: CvService) { }
+
+  ngOnInit(): void {
+    this.formChangesSubscription = this.form.valueChanges!.subscribe(
+      (values) => {
+        if (values.ferrocene_conc < 0 || values.liclo4_conc < 0) {
+          this.error = 'Concentration cannot be negative';
+        } else {
+          this.error = '';
+        }
+      }
+    )
+  }
 }
--- src/app/pages/cv/cv.service.ts
+++ src/app/pages/cv/cv.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Chart } from '../../code/chart';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CvService {
+  public chart: Chart = new Chart('E (V)', 'Current (I)/A', -0.5, 0.6, -4E-6, 8E-6);
+  public ferrocene_conc: number = 1; // mM
+  public liclo4_conc: number = 1; // mM
+  public material: string = 'Pt QRE';
+  public scan_rate: number = 200; // mV/s
+  public min_voltage: number = -0.5;
+  public max_voltage: number = 0.6;
 
   constructor() { }
+
+  public simulate() {
+    // TODO
+  }
 }
