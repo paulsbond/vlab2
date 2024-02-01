ce04ec0d3d58d1147ae6674a17ed91adb97a28f7
Paul Bond <git@paulsbond.co.uk>
2024-02-01 11:52:31 +0000

    Re-ordered imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AlcoholComponent } from './pages/alcohol/alcohol.component';
 import { CaffeineComponent } from './pages/caffeine/caffeine.component';
-import { HomeComponent } from './pages/home/home.component';
-import { SettingsComponent } from './pages/settings/settings.component';
-import { SaltComponent } from './pages/salt/salt.component';
-import { UvvisComponent } from './pages/uvvis/uvvis.component';
 import { CvComponent } from './pages/cv/cv.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SaltComponent } from './pages/salt/salt.component';
+import { SettingsComponent } from './pages/settings/settings.component';
+import { UvvisComponent } from './pages/uvvis/uvvis.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
